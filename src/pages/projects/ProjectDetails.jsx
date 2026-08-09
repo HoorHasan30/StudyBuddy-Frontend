43abc064f266162eed3f7e8315f669a2bf441386
HoorHasan30 <202301820@student.polytechnic.bh>
2026-08-09 22:12:51 +0300

    Feat: Add addCollaberator form

--- src/pages/projects/ProjectDetails.jsx
+++ src/pages/projects/ProjectDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router'
-import { getProjectById } from '../../services/projectService'
+import { getProjectById, addCollaberator, removeCollaberator } from '../../services/projectService'
 import { useAuth } from '../../context/AuthContext'
 
 
@@ -10,49 +10,112 @@ function ProjectDetails() {
 
   const [error, setError] = useState(false)
 
+  const [formData, setFormData] = useState({
+    username: ''
+  });
+
   const [project, setProject] = useState({})
   const { projectId } = useParams()
+  const { user } = useAuth()
 
+  async function handleRemoveCollaberator(collaboratorUsername) {
+    try {
+      await removeCollaberator(projectId, { username: collaboratorUsername })
+      await loadProjectDetails()
+    }
+    catch (err) {
+      setError(err.response.data.message)
+    }
+  }
+
+  async function loadProjectDetails() {
+    try {
+      const response = await getProjectById(projectId)
+      setProject({
+        title: response.title ?? '',
+        description: response.description ?? '',
+        deadline: response.deadline ? new Date(response.deadline).toISOString().slice(0, 10) : '',
+        collaberators: response.collaberators ?? [],
+        tasks: response.tasks ?? [],
+        owner: response.owner ?? ''
+      })
+    }
+    catch (err) {
+      setError(err.response.data.message)
+    }
+  }
+
+  function handleChange(event) {
+    const { name, type, value, checked } = event.target;
+
+    setFormData((prev) => ({
+      ...prev,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      const response = await addCollaberator(projectId, formData)
+      await loadProjectDetails()
+      setFormData({username: ''})
+    }
+    catch (err) {
+      setError(err?.response?.data?.message);
+    }
+  };
 
   useEffect(
     () => {
-      async function loadProjectDetails() {
-        try {
-          const response = await getProjectById(projectId)
-          setProject(response)
-        }
-        catch (err) {
-          setError(err.response.data.message)
-        }
-      }
-
       loadProjectDetails()
     },
     []
   )
 
-  // const projectDeadline = new Intl.DateTimeFormat('en-US', {
-  //   month: 'short',
-  //   day: 'numeric',
-  //   year: 'numeric'
-  // }).format(new Date(project.deadline))
-
-  const allCollaberators = project.collaberators?.length ? project.collaberators.map(c => c) : []
-  const projectTasks = project.tasks?.length ? project.tasks.map(t => t) : []
+  const allCollaberators = project.collaberators?.length ? project.collaberators : []
+  const projectTasks = project.tasks?.length ? project.tasks : []
 
   return (
     <div>
       <h1>{project.title} Details</h1>
+
+      <h3>Add Collaberator: </h3>
+
+      <form autoComplete='off' onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='username'>Username:</label>
+          <input
+            type='text'
+            autoComplete='off'
+            id='username'
+            value={formData.username}
+            name='username'
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button>Add</button>
+
+      </form>
+
+      <hr></hr>
+      
       <p>{project.description}</p>
 
-      <p>Project Deadline: </p>
+      <p>Project Deadline: {project.deadline}</p>
 
       <p>Project Collaberators: </p>
       {allCollaberators.length ? (
         <div>
-          {allCollaberators.map( u => (
+          {allCollaberators.map(u => (
             <div key={`${u._id}`}>
               {u.username}
+
+              {project.owner?._id?.toString() !== user?._id?.toString() ?
+                <button onClick={() => { handleRemoveCollaberator(u.username) }}>Remove</button>
+                : ''}
             </div>
           ))
           }
