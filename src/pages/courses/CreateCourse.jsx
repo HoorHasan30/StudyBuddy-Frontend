f7f8928e71b6d4ab78be8d926eba40010e1cf6a6
WA-2211 <walaaalghanmi@gmail.com>
2026-08-09 15:47:50 +0300

    feat: add course form and fix app.jsx

--- src/pages/courses/CreateCourse.jsx
+++ src/pages/courses/CreateCourse.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router'
+import { createCourse } from '../../services/courseService'
 
 function CreateCourse() {
+  
+  const [formData, setFormData] = useState({
+    title:'',
+    description:'',
+
+  })
+
+  
   return (
     <div>
         <h1>Create Course</h1>
+        <form >
+          <label htmlFor="title">Title:</label>
+          <input type="text" name='title' id='title' value={}/>
+
+          <label htmlFor="description">Description:</label>
+          <input type="textarea" name='description' id='description' value={}/> 
+
+          <button>Create Course</button>         
+
+        </form>
     </div>
   )
 }
