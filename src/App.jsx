2b892680a45fcb7f229088ec0fd0003ca012c433
WA-2211 <walaaalghanmi@gmail.com>
2026-08-09 11:48:23 +0300

    feat: setup

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,32 @@ import CourseDetails from "./pages/courses/CourseDetails";
 import EditCourse from "./pages/courses/EditCourse";
 import { createCourse, getAllCourses, getOneCourse, deleteCourse, updateCourse } from "./services/courseService";
 
+
 import CreateTable from "./pages/timetable/CreateTable";
 import GetTable from "./pages/timetable/GetTable";
 import { getTimetable, createTimeTable, deleteTimetable } from "./services/timetableService";
 
+
 import CreateSession from "./pages/sessions/CreateSession";
 import { createSessions } from "./services/sessionService";
+
 
 import CreateProject from "./pages/projects/CreateProject";
 import AllProjects from "./pages/projects/AllProjects";
 import ProjectDetails from "./pages/projects/ProjectDetails";
 import EditProject from "./pages/projects/EditProject";
+
+import CreateTask from "./pages/tasks/CreateTask";
+import AllTasks from "./pages/tasks/AllTasks";
+import EditTask from "./pages/tasks/EditTask";
+import TaskDetails from "./pages/tasks/TaskDetails";
 import { createProject, getAllProjects } from "./services/projectService";
 
 import { useEffect } from "react";
 import { getCurrentUser, logout } from "./services/authService";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { useAuth } from "./context/AuthContext";
+import EditTask from "./pages/tasks/EditTask";
 function App() {
   return (
     <div>
@@ -44,6 +53,14 @@ function App() {
         <Route path="/courses/:id" element={<ProtectedRoute><CourseDetails/></ProtectedRoute>}/>
         <Route path="/courses/:id/edit" element={<ProtectedRoute><EditCourse/></ProtectedRoute>}/>
 
+
+        <Route path="/courses/:id/tasks" element={<ProtectedRoute><AllTasks/></ProtectedRoute>}/>
+        <Route path="/projects/:id/tasks/create" element={<ProtectedRoute><CreateTask/></ProtectedRoute>}/>
+        <Route path="/projects/:id/tasks/:taskId" element={<ProtectedRoute><TaskDetails/></ProtectedRoute>}/>
+        <Route path="/projects/:id/tasks/:taskId/edit" element={<ProtectedRoute><EditTask/></ProtectedRoute>}/>
+
+
+
         <Route path="/timetable" element={<ProtectedRoute><GetTable/></ProtectedRoute>}/>
         <Route path="/timetable/create" element={<ProtectedRoute><CreateTable/></ProtectedRoute>}/>
 
@@ -53,6 +70,12 @@ function App() {
         <Route path="/projects" element={<ProtectedRoute><AllProjects/></ProtectedRoute>}/>
         <Route path="/projects/:projectId" element={<ProtectedRoute><ProjectDetails/></ProtectedRoute>}/>
         <Route path="/projects/:projectId/edit" element={<ProtectedRoute><EditProject/></ProtectedRoute>}/>
+
+        <Route path="/projects/:projectId/tasks" element={<ProtectedRoute><AllTasks/></ProtectedRoute>}/>
+        <Route path="/projects/:projectId/tasks/create" element={<ProtectedRoute><CreateTask/></ProtectedRoute>}/>
+        <Route path="/projects/:projectId/tasks/:taskId" element={<ProtectedRoute><TaskDetails/></ProtectedRoute>}/>
+        <Route path="/projects/:projectId/tasks/:taskId/edit" element={<ProtectedRoute><EditTask/></ProtectedRoute>}/>
+
 
 
       </Routes>
