4fa92b753a790bca67575f445533d1f251b19748
WA-2211 <walaaalghanmi@gmail.com>
2026-08-09 10:26:57 +0300

    feat: add course setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,17 @@ import SignupPage from "./pages/SignupPage";
 import Homepage from "./pages/Homepage";
 import SignInPage from "./pages/SigninPage";
 import Dashboard from "./pages/Dashboard";
+
+import AllCourses from "./pages/courses/AllCourses";
+import CreateCourse from "./pages/courses/CreateCourse";
+import CourseDetails from "./pages/courses/CourseDetails";
+
+
 import { useEffect } from "react";
 import { getCurrentUser, logout } from "./services/authService";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { useAuth } from "./context/AuthContext";
+import EditCourse from "./pages/courses/EditCourse";
 function App() {
   return (
     <div>
@@ -18,6 +25,13 @@ function App() {
         <Route path="/sign-up" element={<SignupPage />} />
         <Route path="/sign-in" element={<SignInPage />} />
         <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+
+        <Route path="/courses" element={<ProtectedRoute><AllCourses/></ProtectedRoute>}/>
+        <Route path="/courses/create" element={<ProtectedRoute><CreateCourse/></ProtectedRoute>}/>
+        <Route path="/courses/:id" element={<ProtectedRoute><CourseDetails/></ProtectedRoute>}/>
+        <Route path="/courses/:id/edit" element={<ProtectedRoute><EditCourse/></ProtectedRoute>}/>
+
+
       </Routes>
     </div>
   );
---
+++ src/pages/courses/EditCourse.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EditCourse() {
+  return (
+    <div>
+        <h1>EditCourse</h1>
+    </div>
+  )
+}
+
+export default EditCourse
