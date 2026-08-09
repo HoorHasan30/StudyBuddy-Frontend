f00f055fff58d424231d1cfa32da200d64b8517a
WA-2211 <walaaalghanmi@gmail.com>
2026-08-09 16:30:29 +0300

    feat: add all my courses page details and delete course

--- src/pages/courses/AllCourses.jsx
+++ src/pages/courses/AllCourses.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router'
+import { deleteCourse, getAllCourses } from '../../services/courseService'
+import { useAuth } from '../../context/AuthContext'
+
 
 function AllCourses() {
+
+  const { user } = useAuth
+  const [courses, setCourses] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+
+  async function loadCourses() {
+    try {
+      setLoading(true)
+      setError(false)
+
+      const res = await getAllCourses()
+      setCourses(res)
+
+    } catch (error) {
+      setError(error.message)
+    }finally{
+      setLoading(false)
+    }
+   }
+
+   useEffect(()=>{
+    loadCourses()
+   })
+
+
+   async function handleDelete(id){
+    await deleteCourse(id)
+    loadCourses()
+   }
+
+
   return (
     <div>
-        <h1>All Courses</h1>
+      <h1>My Courses</h1>
+      {courses.map((oneCourse)=>
+      <div key={oneCourse._id}>
+        <p>{oneCourse.title}</p>
+        <button onClick={()=>{
+          handleDelete(oneCourse._id)
+        }}>Delete</button>
+      </div>
+      )}
+
+
     </div>
   )
 }
