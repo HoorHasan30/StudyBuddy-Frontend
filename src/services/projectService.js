43ff0225a34b32d13aa8cf540fa0c5e7371ce8d2
HoorHasan30 <202301820@student.polytechnic.bh>
2026-08-09 16:05:41 +0300

    Feat: Add Project Service

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -1,19 +1,52 @@
 import api from "./api";
 
 async function createProject(body) {
-    const res = await api.post('/projects', body)
+    const response = await api.post('/projects', body)
+    return response.data
 
-    return res.data
-    
 }
 
-async function getAllProjects(){
-    const res = await api.get('/projects' )
-    return res.data
+async function getAllProjects() {
+    const response = await api.get('/projects')
+    return response.data
+}
 
+async function getProjectsDeadline() {
+    const response = await api.get('/projects/deadlines')
+}
+
+async function getProjectById(id) {
+    const response = await api.get(`/projects/${id}`)
+    return response.data
+}
+
+async function updateProjectDetails(id, body) {
+    const response = await api.put(`/projects/${id}`, body)
+    return response.data
+}
+
+async function deleteProject(id) {
+    const response = await api.delete(`/projects/${id}`)
+    return response.data
+}
+
+async function addCollaberator(id, body) {
+    const response = await api.post(`/projects/${id}/add-collaberator`, body)
+    return response.data
+}
+
+async function removeCollaberator(id, body) {
+    const response = await api.post(`/projects/${id}/remove-collaberator`, body)
+    return response.data
 }
 
 export {
     createProject,
-    getAllProjects
+    getAllProjects,
+    getProjectsDeadline,
+    getProjectById,
+    updateProjectDetails,
+    deleteProject,
+    addCollaberator,
+    removeCollaberator
 }
