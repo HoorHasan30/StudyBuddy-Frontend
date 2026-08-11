74a8d320c242e39eeb5c2999a1ff3ad99350d575
WA-2211 <walaaalghanmi@gmail.com>
2026-08-11 14:49:16 +0300

    feat: update Homepage styling

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from '../styles/Homepage.module.css'
+import { Link } from 'react-router'
 
 function Homepage() {
   return (
@@ -7,8 +8,11 @@ function Homepage() {
 
       <img className={styles.homeLogo} src="\src\images\7.png" alt="Homepage Logo" />
 
-      <button>Sign-Up</button>
-      <button>Sign-In</button>
+      <div className={styles.linkContainer}>
+        <Link to={'/sign-up'} className={styles.linkSign}>Sign-Up</Link>
+        <Link to={'sign-in'} className={styles.linkSign}>Sign-In</Link>
+
+      </div>
     </div>
   )
 }
